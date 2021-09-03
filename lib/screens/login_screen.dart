59ca9442174299f5af9c91401f7351e266c7008d
muhlikVision <soleh5617@gmail.com>
2021-09-03 18:22:47 +0500

    3sepFinal

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/genericWidgets.dart';
-
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'chat_screen.dart';
 
 
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -2,6 +2,8 @@ import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/genericWidgets.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'reg_screen';
@@ -14,65 +16,75 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
 
   String email;
   String password;
+  bool showSpinner = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            InputField(
-              onChange: (value) {
-                email = value;
-              },
-              bcolor: Colors.blueAccent,
-              text: 'Enter Email',
-              type: TextInputType.emailAddress,
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            InputField(
+              SizedBox(
+                height: 48.0,
+              ),
+              InputField(
                 onChange: (value) {
-                  password = value;
+                  email = value;
                 },
                 bcolor: Colors.blueAccent,
-                text: 'Enter Password',
-                chk: true),
-            SizedBox(
-              height: 24.0,
-            ),
-            ButtonBuilder(
-                onPress: () async {
-                  print(email);
-                  print(password);
-                  try {
-                    final newUser = await _auth.createUserWithEmailAndPassword(
-                        email: email, password: password);
-                    if(newUser != null){
-                      Navigator.pushNamed(context, ChatScreen.id);
+                text: 'Enter Email',
+                type: TextInputType.emailAddress,
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              InputField(
+                  onChange: (value) {
+                    password = value;
+                  },
+                  bcolor: Colors.blueAccent,
+                  text: 'Enter Password',
+                  chk: true),
+              SizedBox(
+                height: 24.0,
+              ),
+              ButtonBuilder(
+                  onPress: () async {
+                    print(email);
+                    print(password);
+                    setState(() {
+                      showSpinner = true;
+                    });
+                    try {
+                      final newUser = await _auth.createUserWithEmailAndPassword(
+                          email: email, password: password);
+                      if(newUser != null){
+                        Navigator.pushNamed(context, ChatScreen.id);
+                      }
+                    } catch (e) {
+                      print(e);
                     }
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-                color: Colors.blueAccent,
-                text: 'Register'),
-          ],
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  },
+                  color: Colors.blueAccent,
+                  text: 'Register'),
+            ],
+          ),
         ),
       ),
     );
